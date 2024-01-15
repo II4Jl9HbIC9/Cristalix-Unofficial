45f516e2d38092afe91dd4cb77f78f313191237c
II4Jl9HbIC9 <140953622+II4Jl9HbIC9@users.noreply.github.com>
2024-01-15 21:45:34 +0100

    Hello World!

--- bot.py
+++ bot.py
@@ -1,17 +1,1 @@
-import hikari
-
-bot = hikari.GatewayBot(token="MTE5MjgyMzA2MzQ5NTA0OTMyNw.GSrekc.Ee0l3yBzqQ343kk9zv9428XSFrP7yYS7d_nFYo")
-
-
-@bot.listen()
-async def ping(event: hikari.GuildMessageCreateEvent) -> None:
-    if not event.is_human:
-        return
-
-    me = bot.get_me()
-
-    if me.id in event.message.user_mentions_ids:
-        await event.message.respond("Нышыта")
-
-
-bot.run()
+# удалил всё шоб глаза не мозолило (токен в истории коммитов давно свапнут, анлаки)
